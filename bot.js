8031cd8144e37bedfeb7f956bb92607c6a62c7e2
Bubbles87 <karden20199@gmail.com>
2025-04-27 23:55:47 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,1 +1,8 @@
-# myrighthand
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.once('ready', () => {
+  console.log('Bot is online!');
+});
+
+client.login('MTM2NTgxNzU1MjUxMjc0OTU3MQ.GT-SgH.WIZozCCfaOjgBvwVFS8pxanVfyNUFb9ufaOBEk');
